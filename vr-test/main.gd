476a45a9ace35fa6266cc7e154dffd6cd53b32f4
RavCarvalho <ravennacarvalho098@gmail.com>
2024-12-17 13:50:18 -0300

    add: novas funções

--- vr-test/main.gd
+++ vr-test/main.gd
@@ -11,5 +11,21 @@ func _ready():
 		DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_DISABLED);
 		
 		get_viewport().use_xr = true;
+		
+		enable_passthrough();
 	else:
 		print("Erro");
+		
+func enable_passthrough() -> bool:
+	var xr_interface: XRInterface = XRServer.primary_interface;
+	if (xr_interface and xr_interface.is_passthrough_supported()):
+		if (!xr_interface.start_passthrough()):
+			return false;
+		else:
+			var modes = xr_interface.get_supported_environment_blend_modes();
+			if (xr_interface.XR_ENV_BLEND_MODE_ALPHA_BLEND in modes):
+				xr_interface.get_environment_blend_modes(xr_interface.XR_ENV_BLEND_MODE_ALPHA_BLEND);
+			else:
+				return false;
+	get_viewport().transparent_bg = true;
+	return true;
